47b89c08c8a653c0161daa4e505567c0bc41e8e3
karlafelix18 <karlafelix1899@gmail.com>
2025-03-10 11:00:00 -0700

    Implementa clases Usuario/Admin/Alumno/Invitado

---
+++ parcial-1-poo/practica-4/clases/Alumno.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'Usuario.php';
+
+class Alumno extends Usuario {
+    private string $matricula;
+
+    public function __construct(string $nombre, string $correo, string $matricula) {
+        parent::__construct($nombre, $correo); // Reutiliza el constructor del padre
+        $this->matricula = $matricula;
+    }
+
+    public function getMatricula(): string { return $this->matricula; }
+    
+    public function getRol(): string { return "Alumno"; }
+}
+?>
