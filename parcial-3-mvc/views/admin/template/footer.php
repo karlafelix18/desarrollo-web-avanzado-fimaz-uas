afb1e8443026cbe5e133318ef6d47f3070ee458e
karlafelix18 <karlafelix1899@gmail.com>
2025-05-05 12:00:00 -0700

    Parcial 3: Templates header y footer del admin

---
+++ parcial-3-mvc/views/admin/template/footer.php
@@ -0,0 +1,32 @@
+</div> <footer class="bg-light text-dark mt-5 border-top">
+        <div class="container pt-4 pb-2">
+            <div class="row text-center small">
+                <div class="col-md-3 mb-3">
+                    <img src="../../img/logo-uas.png" alt="Logo UAS" width="80">
+                </div>
+                <div class="col-md-3 mb-3">
+                    <strong>DESARROLLO WEB AVANZADO</strong><br>
+                    Aplicación Web con PHP, MySQL, PDO y Bootstrap.
+                </div>
+                <div class="col-md-3 mb-3">
+                    <strong>ENLACES DE INTERÉS</strong><br>
+                    <a href="#" class="text-decoration-none text-dark">Reglamento</a><br>
+                    <a href="#" class="text-decoration-none text-dark">Calendario</a>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <strong>CONTACTO</strong><br>
+                    <i class="fa-solid fa-envelope"></i> karlafelix1899@gmail.com<br>
+                    <i class="fa-solid fa-location-dot"></i> Facultad de Informática Mazatlán
+                </div>
+            </div>
+        </div>
+        
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.1);">
+            © <?php echo date("Y"); ?> Copyright:
+            <a class="text-dark fw-bold text-decoration-none" href="#">Karla Paola Felix Del Prado - Ingeniería en Sistemas</a>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
